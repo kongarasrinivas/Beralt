5fad727ad3267c5815c5c705cd5f03876b8b48da
kongarasrinivas <104748608+kongarasrinivas@users.noreply.github.com>
2022-05-02 10:07:21 +0530

    Update BaseTest.java

--- BeraltCRM/src/main/java/com/crm/GenericLib/BaseTest.java
+++ BeraltCRM/src/main/java/com/crm/GenericLib/BaseTest.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-
+//Github Changes
 public abstract class BaseTest implements Iautoconsts {
  
 	public static WebDriver driver;
